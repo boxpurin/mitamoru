15a4b432f1a42b0eb7ed8de4144f4cc0c681afa0
Fumiya Oyama <boxpurin@gmail.com>
2020-10-05 20:17:05 +0900

    作ったけどいらんかったやつら

--- src/filters/filter.rs
+++
@@ -1,9 +0,0 @@
-use warp::Filter;
-
-pub fn hello() -> warp::filters::BoxedFilter<()>{
-  warp::path("hello").boxed()
-}
-
-pub fn name() -> warp::filters::BoxedFilter<(String,)>{
-  warp::path::param().boxed()
-}
--- src/handlers/handler.rs
+++
@@ -1,6 +0,0 @@
-use warp::{Reply, Rejection};
-
-pub async fn greet_handler(name:String) -> Result<impl Reply, Rejection>{
-  let reply = format!("hello {}", name);
-  Ok(warp::reply::html(reply))
-}
